e1e5a0971b90cccedbbdd4053cf69470dfb65849
carlos henrique <carlos.hrq.rodrigues@gmail.com>
2020-12-06 19:05:33 -0300

    :rocket: cliente servidor implementado

---
+++ cliente-8080.py
@@ -0,0 +1,62 @@
+import socket, time, pickle, os
+
+# Cria o socket
+socket_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    socket_client.connect((socket.gethostname(), 8080))
+except:
+    print('ERRO AO CONECTAR COM O SERVIDOR')
+    os._exit(1)
+
+def request(message):
+    try:
+        socket_client.send(message.encode('ascii'))
+        received = socket_client.recv(90000)
+        response = pickle.loads(received)
+        return response
+    except:
+        print('ERRO AO CONECTAR COM O SERVIDOR')
+        os._exit(1)
+
+def opcao_valida(opcao):
+    if opcao > 0 and opcao < 5:
+        return True
+    return False
+
+def escrever_mensagem(response):
+    for r in response:
+        print(r)
+
+while True:
+    print('SELECIONE UMA OPÇÃO')
+    print(1, ' - PARA VERIFICAR USUARIOS CONECTADOS')
+    print(2, ' - PARA ENVIAR UMA MENSAGEM')
+    print(3, ' - LER MENSAGENS')
+    print(4, ' - SAIR')
+
+    opcao_selecionada = int(input())
+    
+    if opcao_valida(opcao_selecionada):
+        if opcao_selecionada == 1:
+            msg = str(1) + '-'
+            response = request(msg)
+            escrever_mensagem(response)
+
+        elif opcao_selecionada == 2:
+            msg = input('Informe a mensagem: \n')
+            msg = str(opcao_selecionada) + '-' + msg
+            request(msg)
+            os.system('cls' if os.name == 'nt' else 'clear')
+
+        elif opcao_selecionada == 3:
+            msg = str(3) + '-'
+            response = request(msg)
+            escrever_mensagem(response)
+          
+        else:
+            msg = str(4) + '-'
+            request(msg)
+            os.system('cls' if os.name == 'nt' else 'clear')
+
+socket_client.close()
---
+++ servidor.py
@@ -0,0 +1,130 @@
+import socket, pickle, threading
+import time, os, platform, subprocess
+from datetime import datetime
+
+os.system('cls' if os.name == 'nt' else 'clear')
+print('BATE PAPO DA UOL ONLINE')
+
+
+porta = 8080
+
+pool = {
+  'connections': [],
+  'posts': [],
+}
+
+def criar_conexao():
+  global porta, pool
+
+  socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+  host = socket.gethostname()
+  socket_server.bind((host, porta))
+  socket_server.listen()
+
+  print("Servidor de nome:", host, " - Aguardando conexão na porta:", porta)
+  (socket_client,addr) = socket_server.accept()
+  print("Conectado a:", str(addr))
+
+  pool['connections'].append([socket_client, socket_server, False, host, porta])
+  porta = porta + 1
+
+def get_response():
+  response = []
+  for post in pool['posts']:
+    response.append(post.to_map())
+
+  return response
+
+def get_request_send_response(client, server, host, port):
+  while True:
+    msg = client.recv(5000)
+    response = 'NoNe'
+    decode = msg.decode('ascii')
+
+    request = decode.split('-')
+    try:
+      if int(request[0]) == 4:
+        break
+    
+      elif int(request[0]) == 1:
+        connections = pool['connections']
+
+        response = []
+        for connection in connections:
+          response.append([connection[3], connection[4]])
+
+      elif int(request[0]) == 2:
+        post = Post(request[1], host, port)
+        pool['posts'].append(post)
+        response = 'OK'
+
+      elif int(request[0]) == 3:
+        response = get_response()
+    except:
+      print('conexão com o cliente na porta: ', port, ' fechada')
+      break
+    
+    bytes_resp = pickle.dumps(response)
+    
+    client.send(bytes_resp)
+
+  client.close()
+  server.close()
+
+class ThreadConnection(threading.Thread):
+  def __init__(self, threadID, name, counter):
+    threading.Thread.__init__(self)
+    self.threadID = threadID
+    self.name = name
+    self.counter = counter
+
+  def run(self):
+    while True:
+      criar_conexao()
+      print ("Connection Established")
+
+class ThreadCommunication(threading.Thread):
+  def __init__(self, threadID, name, counter, server_, client_, host_, port_):
+    threading.Thread.__init__(self)
+    self.threadID = threadID
+    self.name = name
+    self.counter = counter
+    self.server_ = server_
+    self.client_ = client_
+    self.host_ = host_
+    self.port_ = port_
+
+  def run(self):       
+    get_request_send_response(self.client_, self.server_, self.host_, self.port_)
+
+class Post:
+  def __init__(self, msg, host_, port_):
+    self.datetime = str(datetime.now())
+    self.msg = msg
+    self.host_ = host_
+    self.port_ = port_
+  
+  def to_map(self):
+    return {
+      'datetime': self.datetime,
+      'msg': self.msg,
+      'host': self.host_,
+      'port': self.port_
+    }
+
+t = ThreadConnection(1, 'ThreadConnection', 1)
+t.start()
+
+while True:
+  for connection in pool['connections']:
+    connected = connection[2]
+    host = connection[3]
+    port = connection[4]
+
+    if not connected:
+      server = connection[1]
+      client = connection[0]
+
+      connection[2] = True
+      t2 = ThreadCommunication(2, 'ThreadCommunication', 1, server, client, host, port)
+      t2.start()
